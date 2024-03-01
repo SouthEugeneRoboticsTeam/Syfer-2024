ad4081989731ee10c86ff69f68a132cf96088395
Kona <81715373+HeadHunter4621@users.noreply.github.com>
2024-02-29 18:36:34 -0800

    LEDs :3

--- src/main/java/org/sert2521/syfer2024/commands/LEDTest.kt
+++ src/main/java/org/sert2521/syfer2024/commands/LEDTest.kt
@@ -2,20 +2,47 @@ package org.sert2521.syfer2024.commands
 
 import edu.wpi.first.wpilibj2.command.Command
 import org.sert2521.chargedup2023.subsystems.LEDs
+import org.sert2521.syfer2024.Robot
+import org.sert2521.syfer2024.RobotContainer
+import java.lang.System.Logger
 
+private var red = 0
+private var green = 0
+private var blue = 0
+private var totalBright = 0
+
+private var redUp = true
+private var blueUp = true
+private var greenUp = true
+private var time =
 class LEDTest : Command() { //Sets all the LEDs to white as a test to see if they work
-
-    private var red =
     init {
         // each subsystem used by the command must be passed into the addRequirements() method
         addRequirements(LEDs)
     }
 
     override fun initialize() {
-        LEDs.setAllLEDRGB(25, 25, 25)
     }
 
-    override fun execute() {}
+    override fun execute() {
+        println("$totalBright, $g")
+        LEDs.setAllLEDRGB(red, green, blue)
+
+        totalBright = red + green + blue
+
+
+        if(red >= 250){redUp = false}
+        if(green >= 250){greenUp = false}
+        if(blue >= 250){blueUp = false}
+
+        if(red <= 5){redUp = true}
+        if(green <= 5){greenUp = true}
+        if(blue <= 5){blueUp = true}
+
+        if(redUp){red += 5} else {red -= 5}
+        if(greenUp){green += 3} else {green -= 3}
+        if(blueUp){blue += 2} else {blue -= 2}
+    }
 
     override fun isFinished(): Boolean {
         // TODO: Make this return true when this Command no longer needs to run execute()
